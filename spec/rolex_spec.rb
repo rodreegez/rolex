f847a4379b2dc21e97d8ae4fe28aad4dca6c1a0f
Adam <electronicbattleweapon7@gmail.com>
2013-11-26 21:33:35 +0000

    test adding roles and clear up after yourself

--- spec/rolex_spec.rb
+++ spec/rolex_spec.rb
@@ -1,12 +1,29 @@
 require 'spec_helper'
 
+class TestUser
+  include Rolex
+
+  def id ; 1 ; end
+end
+
+
 describe Rolex do
   it 'should have a version number' do
     Rolex::VERSION.should_not be_nil
   end
 
   it 'connect to redis' do
-    puts Rolex.redis.inspect
     Rolex.redis.should_not be_nil
+  end
+
+  it 'should assign a role' do
+    user = TestUser.new
+    user.add_role :admin
+
+    user.has_role?('admin').should be_true
+  end
+
+  after(:each) do
+    Rolex.redis.del 'rolex-test:*'
   end
 end
